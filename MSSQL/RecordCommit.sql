583a00b00c0373255c3541ddadd469ac1bb97809
Humboldt <yan.tai.shih@gmail.com>
2022-07-04 00:11:24 +0800

    20220703

--- MSSQL/RecordCommit.sql
+++ MSSQL/RecordCommit.sql
@@ -25,3 +25,14 @@ update Salary set sex = (case when sex = 'f' then 'm' else 'f' end)
 -- add new item RN sort of class to delete > 1.
 
 WITH ABC AS(SELECT *, RN = ROW_NUMBER()OVER(PARTITION BY email order by id) from Person) DELETE FROM ABC WHERE RN>1
+
+-------------------------------------
+-- # design schem in temp
+CREATE TABLE #temp (  
+    emp_name NVARCHAR(200) NOT NULL,  
+    emp_middlename NVARCHAR(200) NULL,  
+    emp_lastname NVARCHAR(200) NOT NULL  
+);  
+INSERT INTO #temp VALUES( 'Name', NULL, 'Lastname' );  
+SELECT CONCAT( emp_name, emp_middlename, emp_lastname ) AS Result  
+FROM #temp;  
