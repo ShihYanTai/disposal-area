0b1fe89a22aa942550367f58f65d10ee05042572
Humboldt <yan.tai.shih@gmail.com>
2023-06-03 21:44:39 +0800

    20230603_1

---
+++ C# WinForm/Queue/ConsoleApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+
+            queue.Enqueue(100);
+            queue.Enqueue(-25);
+            queue.Enqueue(33);
+            Console.WriteLine($"{queue.Dequeue()}");
+            Console.WriteLine($"{queue.Dequeue()}");
+            Console.WriteLine($"{queue.Dequeue()}");
+        }
+        class Queue
+        {
+            private const int DEFAULTQUESIZE = 100;
+            private int[] data;
+            private int head = 0, tail = 0;
+            private int numElements = 0;
+            public Queue()  => this.data = new int[DEFAULTQUESIZE];
+            public Queue(int size)
+            {
+                if (size > 0) this.data = new int[size];
+                else throw new ArgumentOutOfRangeException("size", "Must be greater than zero");
+            }
+            
+            public void Enqueue(int item)
+            {
+                if(this.numElements == this.data.Length)
+                {
+                    throw new Exception("Queue Full");
+                }
+                this.data[this.head] = item;
+                this.head++;
+                this.head %= DEFAULTQUESIZE;
+                this.numElements++;
+            }
+
+            public int Dequeue()
+            {
+                if(this.numElements ==  0)
+                {
+                    throw new Exception("Queuw empty");
+                }
+
+                int queueItem = this.data[this.tail];
+                this.tail++;
+                this.tail %= this.data.Length;
+                this.numElements--;
+                return queueItem;
+            }
+        }
+    }
+}
---
+++ C# WinForm/binarytree/ConsoleApp3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tree<int> tree1 = new Tree<int>(10);
+            //tree1.Insert(5);
+            //tree1.Insert(11);
+            //tree1.Insert(5);
+            //tree1.Insert(-12);
+            //tree1.Insert(15);
+            //tree1.Insert(0);
+            //tree1.Insert(14);
+            //tree1.Insert(-8);
+            //tree1.Insert(10);
+            //tree1.Insert(8);
+            //tree1.Insert(8);
+
+            Tree<string> tree1 = new Tree<string>("Hello");
+            tree1.Insert("World");
+            tree1.Insert("How");
+            tree1.Insert("Are");
+            tree1.Insert("You");
+            tree1.Insert("Today");
+            tree1.Insert("I");
+            tree1.Insert("Hope");
+            tree1.Insert("You");
+            tree1.Insert("Are");
+            tree1.Insert("Feeling");
+            tree1.Insert("Well");
+            tree1.Insert("!");
+
+            string sortedData = tree1.WalkTree();
+            Console.WriteLine($"Sorted data is: { sortedData}");
+            Console.ReadLine();
+        }
+        public class Tree<TItem> where TItem : IComparable<TItem>
+        {
+            public TItem NodeData { get; set; }
+            public Tree<TItem> LeftTree { get; set; }
+            public Tree<TItem> RightTree { get; set; }
+            public Tree(TItem nodeValue)
+            {
+                this.NodeData = nodeValue;
+                this.LeftTree = null; this.RightTree = null;
+            }
+            public void Insert(TItem newItem)
+            {
+                TItem currentNodeValue = this.NodeData;
+                if (currentNodeValue.CompareTo(newItem) > 0)
+                {
+                    if (this.LeftTree == null) this.LeftTree = new Tree<TItem>(newItem);
+                    else this.LeftTree.Insert(newItem);
+                }
+                else
+                {
+                    if (this.RightTree == null) this.RightTree = new Tree<TItem>(newItem);
+                    else this.RightTree.Insert(newItem);
+                }
+            }
+            public string WalkTree()
+            {
+                string result = "";
+                if (this.LeftTree != null) result = this.LeftTree.WalkTree();
+                result += $"{this.NodeData.ToString()} ";
+                if(this.RightTree != null) result += this.RightTree.WalkTree();
+                return result;
+            }
+        }
+    }
+}
